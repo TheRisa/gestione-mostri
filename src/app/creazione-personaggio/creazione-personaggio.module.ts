3a4da36e36eb6189096e51eaf855d3f813715fd5
dario risaliti <dariorisa@hotmail.it>
2024-11-26 21:16:27 +0100

    interfaccia

--- src/app/creazione-personaggio/creazione-personaggio.module.ts
+++ src/app/creazione-personaggio/creazione-personaggio.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { CreazionePersonaggioPageRoutingModule } from './creazione-personaggio-routing.module';
 
 import { CreazionePersonaggioPage } from './creazione-personaggio.page';
+import { InserisciDatiMostroComponent } from './inserisci-dati-mostro/inserisci-dati-mostro.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CreazionePersonaggioPageRoutingModule
-  ],
-  declarations: [CreazionePersonaggioPage]
+  imports: [CommonModule, FormsModule, IonicModule, CreazionePersonaggioPageRoutingModule],
+  declarations: [CreazionePersonaggioPage, InserisciDatiMostroComponent]
 })
 export class CreazionePersonaggioPageModule {}
--- src/app/creazione-personaggio/inserisci-dati-mostro/inserisci-dati-mostro.component.ts
+++ src/app/creazione-personaggio/inserisci-dati-mostro/inserisci-dati-mostro.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Personaggio } from 'src/app/models';
 
 @Component({
   selector: 'app-inserisci-dati-mostro',
   templateUrl: './inserisci-dati-mostro.component.html',
-  styleUrls: ['./inserisci-dati-mostro.component.scss'],
+  styleUrls: ['./inserisci-dati-mostro.component.scss']
 })
-export class InserisciDatiMostroComponent  implements OnInit {
+export class InserisciDatiMostroComponent implements OnInit {
+  public personaggio: Personaggio = {
+    abilita: [],
+    dur: 0,
+    durMax: 0,
+    hpAttuali: 0,
+    hpMax: 0,
+    nome: '',
+    note: [],
+    pfTmpAttuali: 0,
+    rid: 0,
+    ridMax: 0
+  };
 
-  constructor() { }
+  /** Costruttore della classe */
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  /** Metodo onInit */
+  ngOnInit(): void {}
 
+  public back(): void {
+    this.router.navigate(['tabs', 'creazione-personaggio']);
+  }
 }
